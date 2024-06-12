f7e088c7ef5687b1280c9aa4b8dc73eaf9c01310
kyugamer79 <kyu.gamer79@gmail.com>
2024-06-12 15:06:34 +0330

    update checker fixed

--- inc/functions/cyn-update-checker.php
+++ inc/functions/cyn-update-checker.php
@@ -4,13 +4,13 @@ require(get_stylesheet_directory() . '/inc/libs/plugin-update-checker/plugin-upd
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $updateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/cyandm/yasman_nikzad', //github theme
+	'https://github.com/cyandm/yasaman_nikzad', //github theme
 	get_stylesheet_directory(),
-	'Nikzad' //theme slug
+	'yasaman_nikzad' //theme slug
 );
 
 //Set the branch that contains the stable release.
 $updateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
-$updateChecker->setAuthentication('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');
+// $updateChecker->setAuthentication('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');
